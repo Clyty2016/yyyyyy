70ef7c9a99b7b610adcf36940d95036a1e59f935
tarena <tarena@tarena.local>
2016-06-22 16:32:41 +0800

    Initial Commit

---
+++ Demo5_绘制二维码/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo5_绘制二维码
+//
+//  Created by tarena on 16/6/22.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
